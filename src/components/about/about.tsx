7886d0ee36e74b0334c0ecd79467db7e930cfbaf
chloe <chloechantelle@users.noreply.github.com>
2024-01-29 17:40:39 +1000

    Update spacing and content

--- src/components/about/about.tsx
+++ src/components/about/about.tsx
@@ -7,7 +7,7 @@ export default function About() {
     <div className="about" id="about" style={{ backgroundImage: `url(${cover.src})` }}>
       <div className="about-content">
         <h1 className="about-content-heading">Hello, I'm Lachlan</h1>
-        <p className="about-content-body">I'm an Artistic and creative person driven by my passion for Digital Artistry, Graphic Design and Animation.</p>
+        <p className="about-content-body">I'm an artistic and creative person driven by my passion for Digital Artistry, Graphic Design and Animation.</p>
         <p className="about-content-body">I am proficient with graphic platforms such as the Adobe Suite and possess knowledge with Blender, Figma and digital asset creation of 2D and 3D mediums and their implementation within projects.</p>
         <p className="about-content-body">I'm passionate about UI/UX, Concept design, Animation and other creative ventures, and I'm motivated to learn and grow as a Designer and as an artist.</p>
         <a href="/about">
--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -5,8 +5,8 @@ import TwitterCardMeta from "../components/meta/TwitterCardMeta";
 import header from "../../public/images/aboutheader.jpg";
 import esus from "../../public/images/esusimage.jpeg";
 import { Public } from '@mui/icons-material';
-import { Brush } from '@mui/icons-material';
-import { Palette } from '@mui/icons-material';
+import { RocketLaunchOutlined } from '@mui/icons-material';
+import { AutoAwesomeOutlined } from '@mui/icons-material';
 
 export default function About() {
   return (
@@ -30,11 +30,11 @@ export default function About() {
 
             <p className="about-page-content-body">Lunarnaut, is a fusion of two captivating concepts - "Lunar" and "Astronaut." "Lunar" symbolizes my fascination with space-faring of celestial space beings such as the moon, which represents the mystery and unknowing of the universe, and the endless possibilities that lie beyond our terrestrial boundaries. "Astronaut," on the other hand, embodies the spirit of exploration, pushing boundaries, and venturing into these unknowns; and lastly, the dandelion, the desire to separate and push into uncharted territories and discover new methods and skills to integrate and expand, with these ideals I try to collectively incorporate them with all my designs, to dare to be adventurous and inspire by design.</p>
 
-            <h2><Palette /> Product Design</h2>
+            <h2><RocketLaunchOutlined /> Product Design</h2>
             <p className="about-page-content-body">
               My product design philosophy revolves around creating functional and visually stunning prototypes that enhance user accessibility and flow. Whether it's a simple yet elegant design, or an advanced interface for application use, my designs strive to create a balance of ease and usability to create the best experience for users. Each product I create is carefully crafted to evoke a sense of harmony and balance yet strive to push the boundaries of Product Design.</p>
 
-            <h2><Brush /> Graphic Design</h2>
+            <h2><AutoAwesomeOutlined /> Graphic Design</h2>
             <p className="about-page-content-body">
               In line with my passion for exploration and discovery, I strive to capture the essence of the unknown, transforming ideas into captivating visuals. From brand identities, marketing materials to digital art, I specialize in creating cohesive visual experiences that leave a lasting impression. My designs incorporate bold typography, vivid colours, and awe-inspiring imagery, all crafted to captivate the adventurous souls who seek inspiration.
             </p>
