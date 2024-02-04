7cda4338efa414118e3d951354d0571bedbebdc1
chloe <chloechantelle@users.noreply.github.com>
2024-02-04 17:59:45 +1000

    Update Copyright.tsx

--- src/components/Copyright.tsx
+++ src/components/Copyright.tsx
@@ -1,6 +1,7 @@
 export default function Copyright() {
   return (
     <>
+      <p>In collaboration with <a target="_blank" title="Collaboration" rel="noopener" href="https://www.chloechantelle.com/">CC.</a></p>
       <p>&copy; 2024 Lunarnaut </p>
       <style jsx>
         {`
